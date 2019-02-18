fb9cbc4b4e7c59ae6b694ea070b6c96393ba0ff9
Miguel Ángel León Bardavío <mlb0029@alu.ubu.es>
2019-02-18 09:35:04 +0100

    MetricsEngineException y ValueException
    Relacionado con #42

---
+++ src/main/java/exceptions/MetricsEngineException.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+public class MetricsEngineException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public MetricsEngineException() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public MetricsEngineException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/exceptions/ValueException.java
@@ -0,0 +1,30 @@
+package exceptions;
+
+/**
+ * @author Miguel Ángel León Bardavío - mlb0029
+ *
+ */
+public class ValueException extends Exception {
+
+	/**
+	 * Description.
+	 * 
+	 * @author Miguel Ángel León Bardavío - mlb0029
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public ValueException() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public ValueException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
