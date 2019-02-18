f2086677fe0426283d2cfc3689cb4cde843e61e6
Miguel Ángel León Bardavío <mlb0029@alu.ubu.es>
2019-02-18 11:07:35 +0100

    Implementada métrica TC2
    Relacionado con #39

--- src/main/java/metricsengine/metrics/concrete/MetricDaysBetweenFirstAndLastCommit.java
+++ src/main/java/metricsengine/metrics/concrete/MetricDaysBetweenFirstAndLastCommit.java
@@ -3,21 +3,33 @@
  */
 package metricsengine.metrics.concrete;
 
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import metricsengine.metrics.AMetric;
 import metricsengine.metrics.MetricDescription;
 import metricsengine.values.IValue;
+import metricsengine.values.ValueInteger;
 import repositorydatasource.model.Repository;
 
 /**
- * @author migue
+ * Computes the days between the first and the last commit.
+ * 
+ * @author Miguel Ángel León Bardavío - mlb0029
  *
  */
 public class MetricDaysBetweenFirstAndLastCommit extends AMetric {
 
-	public MetricDaysBetweenFirstAndLastCommit(MetricDescription description, IValue valueMinDefault,
-			IValue valueMaxDefault) {
+	/**
+	 * Constructor of a metric that establishes the description and the default values.
+	 * 
+	 * @param description Description of the metric.
+	 * @param valueMinDefault Minimum value by default.
+	 * @param valueMaxDefault Maximum value by default.
+	 */
+	public MetricDaysBetweenFirstAndLastCommit(MetricDescription description, IValue valueMinDefault, IValue valueMaxDefault) {
 		super(description, valueMinDefault, valueMaxDefault);
-		// TODO Auto-generated constructor stub
 	}
 
 	/* (non-Javadoc)
@@ -25,8 +37,15 @@ public class MetricDaysBetweenFirstAndLastCommit extends AMetric {
 	 */
 	@Override
 	protected Boolean check(Repository repository) {
-		// TODO Auto-generated method stub
-		return null;
+		if (repository != null) {
+			Set<Date> commitDates = repository.getCommitDates();
+			Integer totalNumberOfCommits = repository.getTotalNumberOfCommits();
+			return  commitDates != null &&
+					totalNumberOfCommits != null &&
+					totalNumberOfCommits.intValue() >= 2 &&
+					commitDates.size() == totalNumberOfCommits.intValue();
+		}
+		return false;
 	}
 
 	/* (non-Javadoc)
@@ -34,8 +53,10 @@ public class MetricDaysBetweenFirstAndLastCommit extends AMetric {
 	 */
 	@Override
 	protected IValue run(Repository repository) {
-		// TODO Auto-generated method stub
-		return null;
+		Stream<Date> commitDates = repository.getCommitDates().stream();
+		long firstDate = commitDates.min(Date::compareTo).get().getTime();
+		long lastDate = commitDates.max(Date::compareTo).get().getTime();
+		int result = (int) ((lastDate - firstDate) / (1000 * 60 * 60 * 24));
+		return new ValueInteger(result);
 	}
-
 }
