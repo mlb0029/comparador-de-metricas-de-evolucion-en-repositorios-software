9450fbd81959ce041366518e43352413bb665ba2
Miguel Ángel León Bardavío <mlb0029@alu.ubu.es>
2019-03-01 19:40:03 +0100

    Perfil de métricas por defecto
    Close #40

--- src/main/java/metricsengine/MetricProfile.java
+++ src/main/java/metricsengine/MetricProfile.java
@@ -3,6 +3,7 @@ package metricsengine;
 import java.util.Collection;
 import java.util.HashMap;
 
+import metricsengine.metrics.*;
 import metricsengine.values.IValue;
 
 /**
@@ -33,6 +34,34 @@ public class MetricProfile {
 		this.metricConfigurations = new HashMap<AMetric, MetricConfiguration>();
 	}
 	
+	/**
+	 * Gets the default metric profile defined by the programmer.
+	 * 
+	 * @author Miguel Ángel León Bardavío - mlb0029
+	 * @return The default metric profile.
+	 */
+	public static MetricProfile getDefaultMetricProfile() {
+		MetricProfile defaultMetricProfile = new MetricProfile("Default");
+		
+		MetricTotalNumberOfIssues i1 = new MetricTotalNumberOfIssues();
+		defaultMetricProfile.addMetricConfiguration(i1, i1.getValueMinDefault(), i1.getValueMaxDefault());		
+		MetricCommitsPerIssue i2 = new MetricCommitsPerIssue();
+		defaultMetricProfile.addMetricConfiguration(i2, i2.getValueMinDefault(), i2.getValueMaxDefault());	
+		MetricPercentageClosedIssues i3 = new MetricPercentageClosedIssues();
+		defaultMetricProfile.addMetricConfiguration(i3, i3.getValueMinDefault(), i3.getValueMaxDefault());	
+		MetricAverageDaysToCloseAnIssue ti1 = new MetricAverageDaysToCloseAnIssue();
+		defaultMetricProfile.addMetricConfiguration(ti1, ti1.getValueMinDefault(), ti1.getValueMaxDefault());	
+		MetricAverageDaysBetweenCommits tc1 = new MetricAverageDaysBetweenCommits();
+		defaultMetricProfile.addMetricConfiguration(tc1, tc1.getValueMinDefault(), tc1.getValueMaxDefault());	
+		MetricDaysBetweenFirstAndLastCommit tc2 = new MetricDaysBetweenFirstAndLastCommit();
+		defaultMetricProfile.addMetricConfiguration(tc2, tc2.getValueMinDefault(), tc2.getValueMaxDefault());	
+		MetricChangeActivityRange tc3 = new MetricChangeActivityRange();
+		defaultMetricProfile.addMetricConfiguration(tc3, tc3.getValueMinDefault(), tc3.getValueMaxDefault());	
+		MetricPeakChange c1 = new MetricPeakChange();
+		defaultMetricProfile.addMetricConfiguration(c1, c1.getValueMinDefault(), c1.getValueMaxDefault());	
+		
+		return defaultMetricProfile;
+	}
 	/**
 	 * Gets the name of the profile.
 	 * 
@@ -88,5 +117,4 @@ public class MetricProfile {
 	public void removeMetricConfiguration(MetricConfiguration metricConfiguration) {
 		this.metricConfigurations.remove(metricConfiguration.getMetric());
 	}
-	
 }
